75a48c5e4ffe0e56ed232e1c7b88f686a28a6531
Glenn999 <endgod1987@gmail.com>
2024-01-19 18:39:07 +0800

    清除舊的，保留最新

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -29,7 +29,8 @@ namespace IC20Analyze
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.chkProSearch = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
             this.txtOrign = new System.Windows.Forms.TextBox();
@@ -45,6 +46,7 @@ namespace IC20Analyze
             this.panel3 = new System.Windows.Forms.Panel();
             this.splitter1 = new System.Windows.Forms.Splitter();
             this.btnOpenFile = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -87,11 +89,11 @@ namespace IC20Analyze
             // btnAnalyze
             // 
             this.btnAnalyze.Dock = System.Windows.Forms.DockStyle.Right;
-            this.btnAnalyze.Location = new System.Drawing.Point(602, 0);
+            this.btnAnalyze.Location = new System.Drawing.Point(635, 0);
             this.btnAnalyze.Name = "btnAnalyze";
-            this.btnAnalyze.Size = new System.Drawing.Size(103, 77);
+            this.btnAnalyze.Size = new System.Drawing.Size(70, 77);
             this.btnAnalyze.TabIndex = 10;
-            this.btnAnalyze.Text = "解析";
+            this.btnAnalyze.Text = "重新\r\n解析";
             this.btnAnalyze.UseVisualStyleBackColor = true;
             this.btnAnalyze.Click += new System.EventHandler(this.btnAnalyze_Click);
             // 
@@ -102,14 +104,14 @@ namespace IC20Analyze
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle4;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
@@ -147,7 +149,7 @@ namespace IC20Analyze
             // 
             this.txtPath.Location = new System.Drawing.Point(91, 42);
             this.txtPath.Name = "txtPath";
-            this.txtPath.Size = new System.Drawing.Size(406, 29);
+            this.txtPath.Size = new System.Drawing.Size(447, 29);
             this.txtPath.TabIndex = 66;
             this.txtPath.Tag = "可以先打在這邊";
             // 
@@ -162,11 +164,12 @@ namespace IC20Analyze
             // 
             // btnFileReload
             // 
-            this.btnFileReload.Location = new System.Drawing.Point(503, 4);
+            this.btnFileReload.Location = new System.Drawing.Point(544, 3);
             this.btnFileReload.Name = "btnFileReload";
             this.btnFileReload.Size = new System.Drawing.Size(85, 32);
             this.btnFileReload.TabIndex = 64;
-            this.btnFileReload.Text = "重新載入";
+            this.btnFileReload.Text = "清除重來";
+            this.toolTip1.SetToolTip(this.btnFileReload, "但會保留最新一筆資料");
             this.btnFileReload.UseVisualStyleBackColor = true;
             this.btnFileReload.Click += new System.EventHandler(this.btnFileReload_Click);
             // 
@@ -175,7 +178,7 @@ namespace IC20Analyze
             this.cmbXMLFile.FormattingEnabled = true;
             this.cmbXMLFile.Location = new System.Drawing.Point(221, 6);
             this.cmbXMLFile.Name = "cmbXMLFile";
-            this.cmbXMLFile.Size = new System.Drawing.Size(276, 28);
+            this.cmbXMLFile.Size = new System.Drawing.Size(317, 28);
             this.cmbXMLFile.TabIndex = 63;
             this.cmbXMLFile.Text = "請選擇";
             this.cmbXMLFile.SelectedIndexChanged += new System.EventHandler(this.cmbXMLFile_SelectedIndexChanged);
@@ -211,7 +214,7 @@ namespace IC20Analyze
             // 
             // btnOpenFile
             // 
-            this.btnOpenFile.Location = new System.Drawing.Point(503, 41);
+            this.btnOpenFile.Location = new System.Drawing.Point(544, 40);
             this.btnOpenFile.Name = "btnOpenFile";
             this.btnOpenFile.Size = new System.Drawing.Size(85, 32);
             this.btnOpenFile.TabIndex = 67;
@@ -260,6 +263,7 @@ namespace IC20Analyze
         private System.Windows.Forms.TextBox txtPath;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.ToolTip toolTip1;
     }
 }
 
